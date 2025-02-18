35eed93a7672cc143f23ef404fc46c2c4f438291
Rajvardhan Dangi <rajvardhant563@gmail.com>
2025-02-18 20:43:38 +0530

    Update seekerRoutes.js

--- backend/routes/seekerRoutes.js
+++ backend/routes/seekerRoutes.js
@@ -4,21 +4,21 @@ const router = express.Router()
 const {
         seekerSignup,
         getAllSeekers,
+        getSeekerById,
         updateSeekerDetails,
         deleteSeeker,
         getJobsBySkills,
         applyForJob,
-        searchSeekers,
-        getAppliedJobs
+        searchSeekers
 } = require("../controllers/Seeker");
 
 router.post('/signup' ,seekerSignup);
 router.get('/getall', getAllSeekers);
+router.get('get-seeker/:seekerId', getSeekerById);
 router.put('/update/:seekerId', updateSeekerDetails);
 router.delete('/delete/:seekerId', deleteSeeker);
 router.get('/jobbyskills', getJobsBySkills) 
 router.get('/applyforjob/:seekerId/:jobId', applyForJob);
 router.get('/search', searchSeekers);
-router.get('/applied-jobs/:seekerId', getAppliedJobs);
 
-module.exports = router;
+module.exports = router; 
