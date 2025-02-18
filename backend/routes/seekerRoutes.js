2e8c0265fa080ef69d0521d26b232ca6bcac267e
Rajvardhan Dangi <rajvardhant563@gmail.com>
2025-02-18 21:13:37 +0530

    Update seekerRoutes.js

--- backend/routes/seekerRoutes.js
+++ backend/routes/seekerRoutes.js
@@ -14,7 +14,7 @@ const {
 
 router.post('/signup' ,seekerSignup);
 router.get('/getall', getAllSeekers);
-router.get('get-seeker/:seekerId', getSeekerById);
+router.get('/get-seeker/:seekerId', getSeekerById);
 router.put('/update/:seekerId', updateSeekerDetails);
 router.delete('/delete/:seekerId', deleteSeeker);
 router.get('/jobbyskills', getJobsBySkills) 
