f604263d89451bf2e0afded2078316a2548b2098
Rajvardhan Dangi <rajvardhant563@gmail.com>
2025-02-20 15:08:31 +0530

    Create Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function Navbar() {
+
+    const navigate = useNavigate();
+
+  return (
+    <div className="w-full py-4 bg-blue-950 flex px-8 justify-between">
+        <div className="text-2xl font-semibold pl-8 cursor-pointer" onClick={()=> navigate('/')}>
+            <h1>Job Portal</h1>
+        </div>
+
+        <div className="flex gap-4 text-lg">
+            <p className="cursor-pointer" onClick={()=> navigate('/')}>Home</p>
+            <p className="cursor-pointer" onClick={()=> navigate('/seeker/home')}>Seeker</p>
+            <p className="cursor-pointer" onClick={()=> navigate('/provider/home')}>Provider</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
