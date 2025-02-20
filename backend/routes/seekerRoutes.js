dfe30c1feb8ff04e1b2eeba4139966f5c61280f5
Rajvardhan Dangi <rajvardhant563@gmail.com>
2025-02-20 15:34:27 +0530

    Update seekerRoutes.js

--- backend/routes/seekerRoutes.js
+++ backend/routes/seekerRoutes.js
@@ -9,12 +9,14 @@ const {
         deleteSeeker,
         getJobsBySkills,
         applyForJob,
+        seekerLogin,
         searchSeekers,
         getAppliedJobs
 } = require("../controllers/Seeker");
 
 router.post('/signup' ,seekerSignup);
 router.get('/getall', getAllSeekers);
+router.post('/login', seekerLogin);
 router.get('/get-seeker/:seekerId', getSeekerById);
 router.put('/update/:seekerId', updateSeekerDetails);
 router.delete('/delete/:seekerId', deleteSeeker);
