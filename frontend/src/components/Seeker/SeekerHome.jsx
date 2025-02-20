c9674a1542cdd3f80d54a7e2ca1318d38ce93b6b
Rajvardhan Dangi <rajvardhant563@gmail.com>
2025-02-20 15:13:12 +0530

    Update SeekerHome.jsx

--- frontend/src/components/Seeker/SeekerHome.jsx
+++ frontend/src/components/Seeker/SeekerHome.jsx
@@ -18,7 +18,7 @@ const JobSelector = () => {
           SignUp as Seeker
         </div>
         <div
-          onClick={() => navigate('job-provider/signup')}
+          onClick={() => navigate('/job-seeker/login')}
           to="/job-provider/signup"
           className="w-64 h-16 bg-green-600 hover:bg-green-500 text-xl font-semibold text-white rounded-lg shadow-md transition-all duration-300 flex items-center justify-center gap-2 cursor-pointer"
         >
