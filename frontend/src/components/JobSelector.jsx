0d4d4b6dca4a71e5cd9b8c93cdb81eec0ea8f668
Rajvardhan Dangi <rajvardhant563@gmail.com>
2025-02-20 14:43:33 +0530

    Update JobSelector.jsx

--- frontend/src/components/JobSelector.jsx
+++ frontend/src/components/JobSelector.jsx
@@ -9,14 +9,14 @@ const JobSelector = () => {
     <div className="w-screen h-screen bg-zinc-900 text-white flex flex-col items-center justify-center">
       <div className="w-full max-w-lg p-6 flex flex-col gap-6 bg-zinc-800 rounded-2xl shadow-lg items-center">
         <div
-          onClick = {() => navigate('/job-seeker/signup')}
+          onClick = {() => navigate('/job-seeker/home')}
           to="/job-seeker/signup"
           className="w-64 h-16 bg-indigo-600 hover:bg-indigo-500 text-xl font-semibold text-white rounded-lg shadow-md transition-all duration-300 flex items-center justify-center gap-2 cursor-pointer"
         >
           👨‍💼 Job Seeker
         </div>
         <div
-          onClick={() => navigate('job-provider/signup')}
+          onClick={() => navigate('job-provider/home')}
           to="/job-provider/signup"
           className="w-64 h-16 bg-green-600 hover:bg-green-500 text-xl font-semibold text-white rounded-lg shadow-md transition-all duration-300 flex items-center justify-center gap-2 cursor-pointer"
         >
